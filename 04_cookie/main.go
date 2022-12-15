ef712224f24bf4dc997f4f20266902e9a3c8f289
Jinga <info.alpain@gmail.com>
2022-12-16 06:43:44 +0900

    <clean> リファクタリング

--- 04_cookie/main.go
+++ 04_cookie/main.go
@@ -7,18 +7,17 @@ import (
 )
 
 func main() {
-
 	router := gin.Default()
-
+	// GETメソッド
 	router.GET("/cookie", func(ctx *gin.Context) {
-
+		// Cookieの取得
 		cookie, err := ctx.Cookie("gin_cookie")
-
+		// Cookieがない場合
 		if err != nil {
 			cookie = "NotSet"
+			// Cookieの設定
 			ctx.SetCookie("gin_cookie", "test", 3600, "/", "localhost", false, true)
 		}
-
 		fmt.Printf("Cookie value: %s \n", cookie)
 	})
 
