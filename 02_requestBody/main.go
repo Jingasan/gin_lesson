eae53f2cbcdf06d0d390db73747a17f700220fa8
Jinga <info.alpain@gmail.com>
2022-12-16 06:39:33 +0900

    <clean> リファクタリング

--- 02_requestBody/main.go
+++ 02_requestBody/main.go
@@ -15,11 +15,14 @@ type RequestBodyDataType struct {
 // POSTメソッド処理関数
 func postFunction(ctx *gin.Context) {
 	var json RequestBodyDataType
+	// リクエストボディが規定の型を満たさない場合のエラー処理
 	if err := ctx.ShouldBindJSON(&json); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	ctx.JSON(http.StatusOK, gin.H{"firstname": json.Firstname, "lastname": json.Lastname})
+	// リクエストボディの取得
+	ctx.JSON(http.StatusOK,
+		gin.H{"firstname": json.Firstname, "lastname": json.Lastname})
 }
 
 func main() {
