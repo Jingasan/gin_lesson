a57e88255c001f145fc0e5b55d974cac7e3d5eaf
Jinga <info.alpain@gmail.com>
2022-12-16 06:42:38 +0900

    <clean> リファクタリング

--- 03_URLParameter/main.go
+++ 03_URLParameter/main.go
@@ -10,11 +10,13 @@ func main() {
 	router := gin.Default()
 	// /user/john にはマッチするが、/user/ や /user にはマッチしない
 	router.GET("/user/:name", func(c *gin.Context) {
+		// URLパラメータの取得
 		name := c.Param("name")
 		c.String(http.StatusOK, "Hello %s", name)
 	})
 	// /user/john/ と /user/john/send にマッチする
 	router.GET("/user/:name/*action", func(c *gin.Context) {
+		// URLパラメータの取得
 		name := c.Param("name")
 		action := c.Param("action")
 		message := name + " is " + action
