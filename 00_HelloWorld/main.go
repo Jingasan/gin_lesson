7e3985a44fca52e1a982e76217125a53574d9e08
Jinga <info.alpain@gmail.com>
2022-12-16 06:31:19 +0900

    <clean> リファクタリング

--- 00_HelloWorld/main.go
+++ 00_HelloWorld/main.go
@@ -1,22 +1,28 @@
 package main
 
+// Ginのimport
 import (
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	// POSTメソッド
 	router.POST("/hello", func(ctx *gin.Context) {
 		ctx.String(200, "OK")
 	})
+	// GETメソッド
 	router.GET("/hello", func(ctx *gin.Context) {
 		ctx.String(200, "OK")
 	})
+	// PUTメソッド
 	router.PUT("/hello", func(ctx *gin.Context) {
 		ctx.String(200, "OK")
 	})
+	// DELETEメソッド
 	router.DELETE("/hello", func(ctx *gin.Context) {
 		ctx.String(200, "OK")
 	})
+	// ポート番号を指定
 	router.Run(":3000")
 }
