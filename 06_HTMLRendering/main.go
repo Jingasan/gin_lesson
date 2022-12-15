4aa7c155f27251988452c5442668b19416a87e1d
Jinga <info.alpain@gmail.com>
2022-12-16 06:52:46 +0900

    <clean> リファクタリング

--- 06_HTMLRendering/main.go
+++ 06_HTMLRendering/main.go
@@ -7,16 +7,16 @@ import (
 )
 
 func main() {
-
 	router := gin.Default()
-
+	// router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	// templatesフォルダ下のHTMLファイルを取得
 	router.LoadHTMLGlob("templates/*")
-	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	// GETメソッド
 	router.GET("/index", func(c *gin.Context) {
+		// HTMLにtitleを埋め込み、レスポンスとして返す
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"title": "Main website",
 		})
 	})
-
 	router.Run(":3000")
 }
